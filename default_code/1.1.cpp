dc184095c03cd0e925b84f80379b7fbe749b05e7
nocodenolife3742 <ericncnl3742@gmail.com>
2023-09-13 02:34:01 +0800

    first commit

---
+++ default_code/1.1.cpp
@@ -0,0 +1,50 @@
+// author : 1e-9
+// version : 1.1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// macros
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define rep(i, l, r) for (ll i = (l); i <= ((ll)(r)); i++)
+#define repr(i, r, l) for (ll i = (r); i >= ((ll)(l)); i--)
+#define repin(i, x) for (auto i : (x))
+
+// variables
+typedef long long ll;
+typedef long double ld;
+typedef string str;
+typedef pair<ll, ll> pl;
+typedef vector<ll> vl;
+
+// constants
+#define MOD ((ll)1000000007)
+// #define MOD ((ll)998244353)
+// #define EPS ((ld)1e-6)
+#define EPS ((ld)1e-9)
+// #define INF ((ll)INT_MAX)
+// #define INF ((ll)LONG_LONG_MAX)
+#define INF ((ll)0x3f3f3f3f)
+
+void solve()
+{
+    // solve here
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+    ll cnt = 1;
+    cin >> cnt;
+    while (cnt--)
+    {
+        solve();
+    }
+    return 0;
+}
