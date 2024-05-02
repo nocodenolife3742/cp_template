ca0e6633334b5828997d80983b1f760f54a03c76
Eric <ericncnl3742@gmail.com>
2024-05-03 01:38:32 +0800

    Add min version of rolling hash

--- string/rolling hash.cpp
+++ string/rolling hash.cpp
@@ -1,3 +1,4 @@
+// double hash version
 namespace hs {
     int m = 1e9 + 7;
     int b1 = 999999937; // 999999893, 999999797
@@ -18,10 +19,26 @@ namespace hs {
             }
         }
         poly_hash(string s) : poly_hash(vector<int>(s.begin(), s.end())) {}
-        pair<int, int> operator()(int l, int r) {
+        pair<int, int> operator()(int l, int r) { // [l, r]
             int val1 = (h1[r + 1] - int64_t(h1[l]) * p1[r - l + 1]) % m;
             int val2 = (h2[r + 1] - int64_t(h2[l]) * p2[r - l + 1]) % m;
             return {val1 < 0 ? val1 + m : val1, val2 < 0 ? val2 + m : val2};
         }
     };
 }
+
+// min version
+struct ph {
+    int m = 1e9 + 7, b = 999999937;
+    vector<int> p{1}, h{0};
+    ph(vector<int> arr) {
+        for (int i = 0; i < arr.size(); i++) {
+            p.push_back((int64_t(p[i]) * b) % m);
+            h.push_back((int64_t(h[i]) * b + arr[i]) % m);
+        }
+    }
+    ph(string s) : ph(vector<int>(s.begin(), s.end())) {}
+    int operator()(int l, int r) { // [l, r]
+        return ((h[r + 1] - int64_t(h[l]) * p[r - l + 1]) % m + m) % m;
+    }
+};
