74994da388216ed8bb835b8dea7836876e91ef6f
Eric <123907179+nocodenolife3742@users.noreply.github.com>
2024-06-23 12:55:37 +0800

    Update debug script.cpp for iterable container support

--- c++ skills/debug script.cpp
+++ c++ skills/debug script.cpp
@@ -1,5 +1,22 @@
-void dbg() { cerr << '\n'; }
-template <class A, class... B>
-void debug(A a, B... b) {
-    cerr << a << ' ', debug(b...);
+// macros
+#define TP template
+#define TN typename
+#define iter_print TP<TN T> TN enable_if<is_iter<T>::value, ostream &>::type operator<<(ostream &o, const T &t)
+#define pair_print TP<TN T1, TN T2> ostream &operator<<(ostream &o, const pair<T1, T2> &p)
+TP<TN T> using is_iter = decltype(begin(declval<T>()), end(declval<T>()), true_type());
+// definitions
+iter_print;
+pair_print;
+iter_print {
+    o << '[';
+    for (auto i = t.begin(); i != t.end(); ++i)
+        o << *i << ",]"[next(i) == t.end()];
+    return o;
 }
+pair_print {
+    o << '(' << p.first << ':' << p.second << ')';
+    return o;
+}
+// debug function
+void dbg() { cerr << '\n'; }
+TP<TN T1, TN... T2> void dbg(T1 v1, T2... v2) { cerr << v1 << ' ', dbg(v2...); }
