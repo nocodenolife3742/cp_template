c3908bf2d48a5e11bd295d3d75f3ccece9f6b1ba
Eric <123907179+nocodenolife3742@users.noreply.github.com>
2024-06-24 03:51:39 +0800

    Update debug script.cpp

--- c++ skills/debug script.cpp
+++ c++ skills/debug script.cpp
@@ -1,22 +1,11 @@
 #define TP template
 #define TN typename
-#define iout TP<TN T> TN enable_if<is_iter<T>::value && !is_same<string,\
-             T>::value, ostream &>::type operator<<(ostream &o, const T &t)
-#define pout TP<TN T1, TN T2> ostream &operator<<(ostream &o, const pair<T1, T2> &p)
 TP<TN T> using is_iter = decltype(begin(declval<T>()), end(declval<T>()), true_type());
-
-pout;
-iout;
-pout { return o << '(' << p.first << ':' << p.second << ')'; }
-iout {
-    if (t.empty())
-        return o << "[{empty}]";
-    o << '[';
-    for (auto &&i: t)
-        o << i << ' ';
-    return o << "\b]";
-}
-
+#define IOUT TP<TN T> TN enable_if<is_iter<T>::value && !is_same<string,\
+             T>::value, ostream &>::type operator<<(ostream &o, const T &t)
+#define POUT TP<TN T1, TN T2> ostream &operator<<(ostream &o, const pair<T1, T2> &p)
+POUT; IOUT;
+POUT { return o << '(' << p.first << ':' << p.second << ')'; }
+IOUT { o << '['; for (auto &&i: t) o << i << ' '; return o << (t.empty() ? "]" : "\b]"); }
 void dbg() { cerr << endl; }
-TP<TN T1, TN... T2>
-void dbg(T1 v1, T2... v2) { cerr << v1 << ' ', dbg(v2...); }
+TP<TN T1, TN... T2> void dbg(T1 v1, T2... v2) { cerr << v1 << ' ', dbg(v2...); }
