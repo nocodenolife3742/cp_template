e09edc4cda8eb217017f519955adb2e8272bf58d
Eric <123907179+nocodenolife3742@users.noreply.github.com>
2024-06-24 00:09:04 +0800

    add empty container check implement

--- c++ skills/debug script.cpp
+++ c++ skills/debug script.cpp
@@ -5,19 +5,22 @@
 #define iter_print TP<TN T> TN enable_if<is_iter<T>::value && !is_same<string, T>::value, OS &>::type operator<<(OS &o, const T &t)
 #define pair_print TP<TN T1, TN T2> OS &operator<<(OS &o, const pair<T1, T2> &p)
 TP<TN T> using is_iter = decltype(begin(declval<T>()), end(declval<T>()), true_type());
+
 // definitions
 iter_print;
 pair_print;
 iter_print {
     o << '[';
-    for (auto i = t.begin(); i != t.end(); ++i)
-        o << *i << ",]"[next(i) == t.end()];
+    for (auto &&i : t)
+        o << *i << ' ';
+    o << ']';
     return o;
 }
 pair_print {
     o << '(' << p.first << ':' << p.second << ')';
     return o;
 }
+
 // debug function
 void dbg() { cerr << '\n'; }
 TP<TN T1, TN... T2>
