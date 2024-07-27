091a042b156009299dd20d04b3a676aa261bb099
Eric <123907179+nocodenolife3742@users.noreply.github.com>
2024-07-28 05:48:05 +0800

    Update debug script.cpp

--- c++ skills/debug script.cpp
+++ c++ skills/debug script.cpp
@@ -9,3 +9,19 @@ POUT { return o << '(' << p.first << ':' << p.second << ')'; }
 IOUT { o << '['; for (auto &&i: t) o << i << ' '; return o << (t.empty() ? "]" : "\b]"); }
 void dbg() { cerr << endl; }
 TP<TN T1, TN... T2> void dbg(T1 v1, T2... v2) { cerr << v1 << ' ', dbg(v2...); }
+
+// C++ 17 readable version
+#define dbg(x) cerr << __LINE__ << ": " << #x << " = " << (x) << endl;
+template<typename T1, typename T2>
+ostream &operator<<(ostream &o, const pair<T1, T2> &p);
+template<typename T1, typename T2>
+enable_if_t<is_same_v<T1, ostream>, T1 &> operator<<(T1 &o, const T2 &v) {
+    o << '[';
+    for (auto &&x: v)
+        o << x << ' ';
+    return o << (v.empty() ? "]" : "\b]");
+}
+template<typename T1, typename T2>
+ostream &operator<<(ostream &o, const pair<T1, T2> &p) {
+    return o << '(' << p.first << ':' << p.second << ')';
+}
