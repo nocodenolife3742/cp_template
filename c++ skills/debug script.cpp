2b16db0d3260a8e962a6d45010bc69596de110a3
Eric <123907179+nocodenolife3742@users.noreply.github.com>
2024-07-29 04:03:16 +0800

    Update debug script.cpp

--- c++ skills/debug script.cpp
+++ c++ skills/debug script.cpp
@@ -16,12 +16,13 @@ template<typename T1, typename T2>
 ostream &operator<<(ostream &o, const pair<T1, T2> &p);
 template<typename T1, typename T2>
 enable_if_t<is_same_v<T1, ostream>, T1 &> operator<<(T1 &o, const T2 &v) {
-    o << "[ ";
-    for (auto &&i: v)
-        o << i << ' ';
+    o << '[';
+    for (auto i = v.begin(); i != v.end(); ++i)
+        o << (i != v.begin() ? " " : "") << *i;
     return o << ']';
 }
 template<typename T1, typename T2>
 ostream &operator<<(ostream &o, const pair<T1, T2> &p) {
     return o << '(' << p.first << ':' << p.second << ')';
 }
+
