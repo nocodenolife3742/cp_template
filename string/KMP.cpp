064d91054be878caa5b8757d9abbafc222e8e823
Eric <123907179+nocodenolife3742@users.noreply.github.com>
2024-07-30 07:08:30 +0800

    Update KMP.cpp

--- string/KMP.cpp
+++ string/KMP.cpp
@@ -1,24 +1,8 @@
-vector<int> kmp(string &s, string &t) {
-    if (t.size() == 0)
-        return {};
-    vector<int> pos;
-    vector<int> nxt(t.size(), 0);
-    for (int i = 1, j = 0; i < t.size(); i++) {
-        while (j > 0 && t[i] != t[j])
-            j = nxt[j - 1];
-        if (t[i] == t[j])
-            j++;
-        nxt[i] = j;
+vector<int> kmp_next(string &s) {
+    vector<int> nxt(s.size(), 0);
+    for (int i = 1, j = 0; i < s.size(); nxt[i++] = j) {
+        while (j > 0 && s[i] != s[j]) j = nxt[j - 1];
+        if (s[i] == s[j]) j++;
     }
-    for (int i = 0, j = 0; i < s.size(); i++) {
-        if (j > 0 && s[i] != t[j])
-            j = nxt[j - 1];
-        if (s[i] == t[j])
-            j++;
-        if (j == t.size()) {
-            pos.push_back(i - t.size() + 1);
-            j = nxt[j - 1];
-        }
-    }
-    return pos;
+    return nxt;
 }
