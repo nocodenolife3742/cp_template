654ac5ea556619568f55ef3a82db6c533c0b8d1f
Eric <123907179+nocodenolife3742@users.noreply.github.com>
2024-08-14 18:05:10 +0800

    Update segment tree.cpp

--- data structure/segment tree.cpp
+++ data structure/segment tree.cpp
@@ -1,66 +1,74 @@
-// 1. implement merge, apply function
-// 2. initialize data and lazy of node
-// 3. decide default lazy tag
+struct range {
+    int l, r;
+    bool contain(range &o) { return l <= o.l && o.r <= r; }
+    bool intersect(range &o) { return l <= o.r && o.l <= r; }
+    pair<range, range> split() {
+        int mid = l + ((r - l) >> 1);
+        return {{l, mid}, {mid + 1, r}};
+    }
+};
 template<typename T1, typename T2>
 struct segtree {
     static T1 merge(T1 a, T1 b) {
-        return a + b;
+        // merge two values
     }
     struct node {
         T1 data;
-        T2 lazy;
+        T2 tag;
+        range seg;
         node *lc, *rc;
-        void apply(T2 t, int l, int r) {
-            data += (r - l + 1) * t;
-            lazy += t;
+        bool upd;
+        node(range s) : seg(s), upd(false) {
+            lc = rc = nullptr;
+            // init data and tag
+        }
+        void apply(T2 t) {
+            upd = true;
+            // apply tag to node
         }
         void up() {
-            if (lc) data = merge(lc->data, rc->data);
+            if (lc && rc)
+                data = merge(lc->data, rc->data);
         }
-        void down(int l, int r) {
-            if (lc) {
-                int m = (l + r) / 2;
-                lc->apply(lazy, l, m);
-                rc->apply(lazy, m + 1, r);
-            }
-            lazy = 0;
+        void down() {
+            if (lc && rc && upd)
+                lc->apply(tag), rc->apply(tag);
+            upd = false;
+            // clear tag
         }
     } *root;
-    int n;
-    node *build(int l, int r) {
-        node *p = new node();
-        if (l == r) return p;
-        int m = (l + r) / 2;
-        p->lc = build(l, m);
-        p->rc = build(m + 1, r);
+    range a;
+    node *build(range s) {
+        node *p = new node(s);
+        if (s.l == s.r) return p;
+        auto [l, r] = s.split();
+        p->lc = build(l);
+        p->rc = build(r);
         return p->up(), p;
     }
-    void modify(int a, int b, int l, int r, node *p, T2 x) {
-        if (r < a || l > b) return;
-        p->down(l, r);
-        if (a <= l && b >= r)
-            return p->apply(x, l, r);
-        int m = (l + r) / 2;
-        modify(a, b, l, m, p->lc, x);
-        modify(a, b, m + 1, r, p->rc, x);
+    void modify(range s, range t, node *p, T2 x) {
+        if (!s.intersect(t)) return;
+        p->down();
+        if (t.contain(s)) return p->apply(x);
+        auto [l, r] = s.split();
+        modify(l, t, p->lc, x), modify(r, t, p->rc, x);
         p->up();
     }
-    T1 query(int a, int b, int l, int r, node *p) {
-        p->down(l, r);
-        if (a <= l && b >= r) return p->data;
-        int m = (l + r) / 2;
-        if (b <= m) return query(a, b, l, m, p->lc);
-        if (a > m) return query(a, b, m + 1, r, p->rc);
-        return merge(query(a, b, l, m, p->lc),
-                     query(a, b, m + 1, r, p->rc));
+    T1 query(range s, range t, node *p) {
+        p->down();
+        if (t.contain(s)) return p->data;
+        auto [l, r] = s.split();
+        if (!r.intersect(t)) return query(l, t, p->lc);
+        if (!l.intersect(t)) return query(r, t, p->rc);
+        return merge(query(l, t, p->lc), query(r, t, p->rc));
     }
-    segtree(int size) : n(size) {
-        root = build(0, n - 1);
+    segtree(int n) : a({0, n - 1}) {
+        root = build(a);
     }
-    void modify(int l, int r, T2 x) {
-        modify(l, r, 0, n - 1, root, x);
+    void modify(range t, T2 x) {
+        modify(a, t, root, x);
     }
-    T1 query(int l, int r) {
-        return query(l, r, 0, n - 1, root);
+    T1 query(range t) {
+        return query(a, t, root);
     }
 };
